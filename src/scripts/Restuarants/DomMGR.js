2983abbe1450376e1ee7080bfa0d435be95467c2
Stephen Senft <stephensenft21@gmail.com>
2019-03-14 14:20:33 -0500

    made changes to element on html

--- src/scripts/Restuarants/DomMGR.js
+++ src/scripts/Restuarants/DomMGR.js
@@ -1,24 +1,24 @@
  console.log("test")
 
- // user input fields 
-//  const restuarantInputField = document.createElement("Input");
-//  restuarantInputField.id = "restuarantInput"
-//  const parksInputField = document.createElement("Input");
-//  parksInputField.id = "parksInput"
+ 
+ const restuarantInputField = document.createElement("Input");
+ restuarantInputField.id = "restuarantInput"
+ const parksInputField = document.createElement("Input");
+ parksInputField.id = "parksInput"
 
-//  const meetupsInputField = document.createElement("Input");
-//  meetupsInputField.id = "meetupsInput"
-//  const concertsInputField = document.createElement("Input");
-//  concertsInputField.id = "concertsInput"
-
-
-//  const restaurantButton = document.createElement("button");
-//  const parksButton = document.createElement("button");
-//  const meetupsButton = document.createElement("button");
-//  const searchButton = document.createElement("button");
+ const meetupsInputField = document.createElement("Input");
+ meetupsInputField.id = "meetupsInput"
+ const concertsInputField = document.createElement("Input");
+ concertsInputField.id = "concertsInput"
 
 
- let displayContainer = document.querySelector("#container")
+ const restaurantButton = document.createElement("button");
+ const parksButton = document.createElement("button");
+ const meetupsButton = document.createElement("button");
+ const searchButton = document.createElement("button");
+
+
+ let displayContainer = document.querySelector("#display-container")
  const formContainer = () => {
      const mainSection = document.createElement("section")
      const articleSection = document.createElement("article")
