2fbfef1aa9b04c0dbdc41d81812c7afe5e52f559
anneraev <47757104+anneraev@users.noreply.github.com>
2019-03-14 15:46:14 -0500

    fixed index and created buttons DOM manager

--- src/scripts/concerts/domManager.js
+++ src/scripts/concerts/domManager.js
@@ -1,6 +1,62 @@
-const section = document.querySelector("#display-container");
+//containers for input/button pairs.
+const restaurantSearch = document.createElement("div")
+restaurantSearch.id = "restaurantBox"
+const parksSearch = document.createElement("div")
+parksSearch.id = "parksBox"
+const meetupsSearch = document.createElement("div")
+meetupsSearch.id = "meetupsBox"
+const concertsSearch = document.createElement("div")
+concertsSearch.id = "concertsBox"
+
+// user input fields 
+const restuarantInputField = document.createElement("Input");
+restuarantInputField.id = "restuarantInput"
+const parksInputField = document.createElement("Select");
+parksInputField.id = "parksInput"
+parksInputField.name = "parksInput"
+const meetupsInputField = document.createElement("Input");
+meetupsInputField.id = "meetupsInput"
+const concertsInputField = document.createElement("Input");
+concertsInputField.id = "concertsInput"
+
+//search buttons
+const restaurantButton = document.createElement("button");
+const parksButton = document.createElement("button");
+const meetupsButton = document.createElement("button");
+const concertsButton = document.createElement("button");
+concertsButton.innerHTML = "Search"
+restaurantButton.innerHTML = "Search"
+meetupsButton.innerHTML = "Search"
+parksButton.innerHTML = "Search"
+
+//append search inputs and buttons to appropriate boxes.
+restaurantSearch.appendChild(restuarantInputField);
+restaurantSearch.appendChild(restaurantButton);
+parksSearch.appendChild(parksInputField);
+parksSearch.appendChild(parksButton);
+concertsSearch.appendChild(concertsInputField);
+concertsSearch.appendChild(concertsButton);
+meetupsSearch.appendChild(meetupsInputField);
+meetupsSearch.appendChild(meetupsButton);
+
+//reference to container section
+const section = document.querySelector("#container");
+console.log(section);
+
+//document fragment
 const fragment = document.createDocumentFragment();
-console.log(section)
+
+//append container boxes to DOM fragment.
+fragment.appendChild(restaurantSearch);
+fragment.appendChild(concertsSearch);
+fragment.appendChild(meetupsSearch);
+fragment.appendChild(parksSearch);
+
+//append fragment to the container section container.
+section.appendChild(fragment);
+
+////////////
+
 
 const textToHTML = (event, place) => {
     return `
@@ -8,13 +64,10 @@ const textToHTML = (event, place) => {
     `
 }
 
-const resultsBox = () => {
-}
+const resultsBox = () => {}
 
 //passes list element to append save button to.
 const saveButton = (elment) => {
     //create button element
     //append to saved dom element
 }
-
-
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,20 @@
-const concertButton = document.getElementById("concert-button");
-const restaurantButton = document.getElementById("restaurant-button");
-const meetupButton = document.getElementById("meetups-button");
-const parkButton = document.getElementById("park-button");
-const parkSearch = document.getElementById("park");
-const restaurantSearch = document.getElementById("restaurant");
-const concertSearch = document.getElementById("concert");
-const meetupsSearch = document.getElementById("meetups");
-
-console.log(parkButton);
-
 const eventSearch = (term) => {
     console.log(term);
-//pass input to API lookup
-ticketmasterLog(term);
-//pass returned values to dom manager for posting
+    //pass input to API lookup
+    ticketmasterLog(term);
+    //pass returned values to dom manager for posting
 }
 
-concertButton.addEventListener("click", () => {
-    eventSearch(concertSearch.value)
+
+concertsButton.addEventListener("click", () => {
+    eventSearch(concertsInputField.value)
 })
 restaurantButton.addEventListener("click", () => {
-    eventSearch(restaurantSearch.value)
+    eventSearch(concertsInputField.value)
 })
-meetupButton.addEventListener("click", () => {
-    eventSearch(meetupsSearch.value)
+meetupsButton.addEventListener("click", () => {
+    eventSearch(meetupsInputField.value)
 })
-parkButton.addEventListener("click", () => {
-    eventSearch(parkSearch.value)
+parksButton.addEventListener("click", () => {
+    eventSearch(parksInputField.value)
 })
