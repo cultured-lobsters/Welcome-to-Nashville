1363a8417d80e7110b450aa90c900b7e2e88ec76
anneraev <47757104+anneraev@users.noreply.github.com>
2019-03-14 11:51:41 -0500

    created basic input return value function

--- src/scripts/concerts/script.js
+++ src/scripts/concerts/script.js
@@ -1,7 +1,7 @@
 //when called, fetches data for the genre passed to it by the function call.
 const ticketmasterCall = (genre) => {
     return fetch(
-            `https://app.ticketmaster.com/discovery/v2/events.json?apikey=iGNHGX3JLkYElcOGNL7J2qMZl02a5nBU&city=Nashville&countryCode=US&classificationName=${genre}`
+            `https://app.ticketmaster.com/discovery/v2/events.json?apikey=iGNHGX3JLkYElcOGNL7J2qMZl02a5nBU&city=Nashville&size=4&countryCode=US&classificationName=${genre}`
         )
         .then(concerts => concerts.json())
 };
@@ -10,10 +10,10 @@ const ticketmasterCall = (genre) => {
 const ticketmasterLog = (genre) => {
     ticketmasterCall(genre).then(parsedConcerts => {
         console.log(parsedConcerts);
+                //if the below returns undefined, need alert to appear on page to tell user they must input a proper value.
+        console.log(parsedConcerts._embedded);
         //Return array of events, following the data structure in the API, each item nested in the data that prceeds it in the data path.
         const concertInfosArray = parsedConcerts._embedded.events;
-        //if the below returns undefined, need alert to appear on page to tell user they must input a proper value.
-        console.log(parsedConcerts._embedded);
         //Array of concerts.
         //For each concert, get variable with concert name and venue name sourced from the API database.
         concertInfosArray.forEach(concert => {
@@ -23,8 +23,8 @@ const ticketmasterLog = (genre) => {
             const venueName = concert._embedded.venues[0].name;
             console.log(venueName);
             console.log(`${concertName} at ${venueName}`)
+            const HTMLitem = textToHTML(concertName, venueName);
+            console.log(HTMLitem)
         });
     });
 }
-
-ticketmasterLog("pop");
